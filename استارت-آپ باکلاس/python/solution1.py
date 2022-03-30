3d12becd7f562a53b914004a767b6d45f9556dcf
mohammadnpak <m.nozaripak@gmail.com>
2022-03-30 12:22:43 +0430

    1401-01-09

--- استارت-آپ باکلاس/python/solution1.py
+++ استارت-آپ باکلاس/python/solution1.py
@@ -1,2 +1,13 @@
 # https://quera.org/problemset/10326/
 
+w = [int(x) for x in input().split()]
+person = [0]*4
+n=0
+while 0 not in w:
+  person[n%4]+=1
+  if (n%4)%2==0:
+    w[0]-=1
+  else:
+    w[2]-=1
+  n+=1
+print(*person)
--- گردو شکستم/python/solution1.py
+++ گردو شکستم/python/solution1.py
@@ -1,3 +1,12 @@
 # https://quera.org/problemset/3540/
 
 n,x,y = map(int,input().split())
+# ax+by=n => a = (n-by)/x  => a must be an integer
+b=0
+while b*y <= n:
+  if (n-b*y)%x==0:
+    print((n-b*y)//x,b)
+    break
+  b+=1
+else:
+  print(-1)
