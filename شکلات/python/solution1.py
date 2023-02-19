85114efbc40e980a80d56f456ea42143e6b0be73
hosseiinaliizadeh <h.alizadeh2004@gmail.com>
2023-02-19 10:13:22 +0330

    add 3 python problems to github(quera.ir)

---
+++ شکلات/python/solution1.py
@@ -0,0 +1,5 @@
+inpt = input().split()
+n, k = int(inpt[0]), int(inpt[1])
+
+if n % k == 0: print('YES')
+else: print('NO')
---
+++ پشمال‌آقا/python/solution1.py
@@ -0,0 +1,5 @@
+inp = input()
+inp = inp.split()
+n = int(inp[0]); k = float(inp[1]); p = int(inp[2])
+price = int(n * k * p)
+print(price)
